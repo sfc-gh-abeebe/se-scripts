c54e47efabebe956d8aa3c2c6be06ba503899ce3
sfc-gh-abeebe <amber.beebe@snowflake.com>
2023-01-12 14:29:19 -0800

    Upload split_large_json
    Utility to workaround the 16mb file size limitation.

---
+++ split_large_json/README.md
@@ -0,0 +1,54 @@
+## Overview
+Today, Snowflake has a ingest file size limit of 16MB. This applies to selecting from stages as well as loading large JSON documents into a variant column.
+This folder contains the following scripts:
+* split_json.sql
+* json-simple-1.1.1.jar
+
+## Setup Instructions
+1. Upload the dependencies to a external stage or to an internal Snowflake stage
+
+### Example Using SnowSQL
+```
+put file:///Users/abeebe/Downloads/GitHub/se-scripts/split_large_json/json-simple-1.1.1.jar @~/stage/ auto_compress=false;
+```
+2. Create the function using the code in the `split_json.sql` file
+3. Execute the function using the following SQL
+
+```
+select OBJECT_INSERT(root, 'orders', node)
+from table(StripInnerArray('@~/staged/my_large_json_file.json','orders'));
+```
+
+The function `StripInnerArray` takes two arguments. The first will be the location of the file (including stage reference). The second is the parent element of the JSON object to start parsing at.
+
+For example, say we have a JSON object like the following:
+```
+obj = {
+    "firstName": "Amber",
+    "lastName": "Beebe",
+    "email": "amber.beebe@snowflake.com",
+    "orders": {
+        1000: {
+            "productSKU": 123,
+            "orderDate": "2022-12-27"
+        },
+        1001: {
+            "productSKU": 254,
+            "orderDate": "2023-01-04"
+        },
+        1002: {
+            "productSKU": 999,
+            "orderDate": "2023-01-12"
+        }
+    }   
+}
+```
+We want to flatten the nested objects in the "orders" object to show one order per row, however the file `my_large_json_file.json` is larger than 16MB and Snowflake will not allow us to transform in flight or ingest.
+
+If we call the function, as demonstrated above, the result will be:
+
+```
+{   "email": "amber.beebe@snowflake.com",   "firstName": "Amber",   "lastName": "Beebe",   "orders": {     "orderDate": "2023-01-12",     "productSKU": 999   } },
+{   "email": "amber.beebe@snowflake.com",   "firstName": "Amber",   "lastName": "Beebe",   "orders": {     "orderDate": "2023-01-04",     "productSKU": 254   } },
+{   "email": "amber.beebe@snowflake.com",   "firstName": "Amber",   "lastName": "Beebe",   "orders": {     "orderDate": "2022-12-27",     "productSKU": 123   } },
+```
---
+++ split_large_json/split_json.sql
@@ -0,0 +1,64 @@
+create or replace function StripInnerArray(Fname varchar, Node varchar)                           
+returns table(root variant, node variant)                                                             
+language java                                                                
+imports = ('@~/staged/json-simple-1.1.1.jar')
+handler='StripInnerArray'                                                 
+target_path='@~/staged/StripInnerArray.jar'                                                                
+as                                                                  
+$$  
+    import java.io.BufferedReader;
+    import java.io.InputStream;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.nio.charset.StandardCharsets;
+    import java.util.ArrayList;
+    import java.util.Iterator;
+    import java.util.List;
+    import java.util.stream.Stream;
+
+    import org.json.simple.*;
+    import org.json.simple.parser.JSONParser;
+
+     class StripInnerArray {
+       class OutputRow {                                      
+         public String root;
+         public String node;
+          public OutputRow(String x, String y) {
+            this.root = (String) x;
+            this.node = (String) y;
+          }
+       }                       
+        public static Class getOutputClass() {
+            return OutputRow.class;
+        }
+        public Stream<OutputRow> process(InputStream stream, String Node) throws Exception {
+            BufferedReader jsonBuffer = new BufferedReader( new InputStreamReader(stream, StandardCharsets.UTF_8));
+
+            StringBuilder contentBuilder = new StringBuilder();
+            jsonBuffer.lines().forEach(s -> contentBuilder.append(s).append("\n"));
+            stream.close();
+
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(contentBuilder.toString());
+
+            List<JSONObject> objs = new ArrayList<JSONObject>();
+
+            JSONObject node = (JSONObject) jsonObject.get(Node);
+            Iterator<String> x = node.keySet().iterator();
+
+            while (x.hasNext()) {
+                String key = (String) x.next();
+                objs.add((JSONObject) node.get(key));
+            }
+
+            jsonObject.remove(Node);
+
+            return objs.stream().map(s -> new OutputRow(jsonObject.toString(), s.toString().replace("\\/", "/")));
+        }
+    }
+$$;
+
+// call the function and build the final variant object
+// you could leave the OBJECT_INSERT call out entirely
+select OBJECT_INSERT(root, 'ros', node)
+from table(StripInnerArray('@~/staged/my_large_json_file.json','key_values'));
